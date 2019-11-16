e52e50788e3d47608a2b91b0604de93927ac773e
Matthew Nelson <05nelsonm@protonmail.com>
2019-11-15 19:28:15 -0500

    Initialized IntelliJ project

---
+++ src/main/java/com/zero5nelsonm/lendr/LendrApplication.java
@@ -0,0 +1,13 @@
+package com.zero5nelsonm.lendr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero5nelsonm/lendr/LendrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero5nelsonm.lendr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
